634dee4d76a22b94b6f8072c194c0052001b5abb
unknown <pvl01.main@gmail.com>
2020-11-29 20:08:29 -0500

    first commit

--- src/daos/questions-dao.js
+++ src/daos/questions-dao.js
@@ -1,5 +1,5 @@
 const questionsModel = require('../models/questions/questions-model.js')
 const findAllQuestions= () => questionsModel.find()
 const findQuestionById = (qid) => questionsModel.findById(qid)
-const findQuestionsForQuiz = (qzid) => questionsModel.find({quizId: qzid})
+const findQuestionsForQuiz = (qid) => questionsModel.find({quizId: qid})
 module.exports = { findAllQuestions, findQuestionById }
